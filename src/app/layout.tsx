5338bd4c521e8bf3e8804607a00d0d5a25d40552
IbcFan <ibcfan@proton.me>
2024-03-13 13:14:42 -0700

    Add login page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "~/styles/globals.css";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from '@clerk/nextjs'
 import { TRPCReactProvider } from "~/trpc/react";
+import Navbar from "@/app/_components/NavBar";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -24,9 +25,19 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`font-sans ${inter.variable}`}>
-          <TRPCReactProvider>{children}</TRPCReactProvider>
+        <TRPCReactProvider>
+          <Navbar/>
+          <main className="flex min-h-screen flex-col items-center justify-center">
+            <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+              <div className="w-full max-w-lg">
+                {children}
+              </div>
+            </div>
+          </main>
+
+        </TRPCReactProvider>
         </body>
       </html>
     </ClerkProvider>
-  );
+);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,10 @@
 import { unstable_noStore as noStore } from "next/cache";
+import { AnalyseChannel } from "@/app/_components/analyze-channel";
 
-import { AnalyseChannel } from "~/app/_components/analyze-channel";
 export default async function Home() {
   noStore();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <div className="w-full max-w-lg">
-          <AnalyseChannel />
-        </div>
-      </div>
-    </main>
+    <AnalyseChannel/>
   );
 }
