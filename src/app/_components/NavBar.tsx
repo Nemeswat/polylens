5e422aa88475712da662b1a0e19af3276245ea61
eigenvibes <aneeshsachdeva1+git@gmail.com>
2024-03-14 16:25:36 -0700

    Update nav bar layout

--- src/app/_components/NavBar.tsx
+++ src/app/_components/NavBar.tsx
@@ -1,4 +1,5 @@
-"use client";
+'use client';
+
 import Link from "next/link";
 import React, { useState } from "react";
 import { XMarkIcon, Bars3Icon } from "@heroicons/react/24/solid";
@@ -9,7 +10,7 @@ import {
 } from "@clerk/nextjs";
 
 const routes: { title: string; href: string }[] = [
-  // { title: "Features", href: "#features" },
+  { title: "Dashboard", href: "/dashboard" },
 ];
 
 const Navbar: React.FC = () => {
@@ -25,20 +26,22 @@ const Navbar: React.FC = () => {
         <Link href={"/"} className="shrink-0">
           <h1 className="text-accent-foreground text-2xl font-bold">polylens</h1>
         </Link>
-        <div className="bg-background hidden w-full justify-end gap-1 px-4 py-2 sm:flex">
-          {routes.map((route, index) => (
-            <Link
-              key={index}
-              href={route.href}
-              className={`hover:text-accent-foreground text-muted-foreground inline-flex h-10 w-full items-center px-4 py-2 text-sm transition-colors sm:w-auto`}
-            >
-              {route.title}
-            </Link>
-          ))}
+        <div className="bg-background hidden w-full justify-end gap-4 px-4 py-2 sm:flex">
+          <SignedIn>
+            {routes.map((route, index) => (
+              <Link
+                key={index}
+                href={route.href}
+                className={`hover:text-accent-foreground text-muted-foreground inline-flex h-10 w-full items-center px-4 py-2 text-sm transition-colors sm:w-auto`}
+              >
+                {route.title}
+              </Link>
+            ))}
+          </SignedIn>
         </div>
       </div>
 
-      <div className="hidden items-center gap-2 sm:flex">
+      <div className="hidden items-center gap-4 sm:flex">
         <SignedOut>
           <Link href={"/sign-in"} className="w-full sm:w-auto">
             <Button variant="secondary" size="sm" className="w-full">
@@ -48,14 +51,8 @@ const Navbar: React.FC = () => {
         </SignedOut>
 
         <SignedIn>
-          <Link href="/dashboard" className="w-full sm:w-auto">
-            <Button variant="default" size="sm" className="w-full">
-              Dashboard
-            </Button>
-          </Link>
           <UserButton />
         </SignedIn>
-
       </div>
 
       {menuOpen && <MobileMenu toggleMenu={toggleMenu} />}
@@ -74,17 +71,19 @@ const Navbar: React.FC = () => {
 const MobileMenu: React.FC<{ toggleMenu: () => void }> = ({ toggleMenu }) => {
   return (
     <div className="absolute right-0 top-16 flex h-[calc(100vh-64px)] w-full flex-col">
-      <div className="bg-background  flex w-full grow flex-col gap-1 px-4 pb-2">
-        {routes.map((route, index) => (
-          <Link
-            key={index}
-            href={route.href}
-            onClick={toggleMenu}
-            className={`hover:text-accent-foreground text-muted-foreground inline-flex h-10 w-full items-center text-sm transition-colors sm:w-auto`}
-          >
-            {route.title}
-          </Link>
-        ))}
+      <div className="bg-background  flex w-full grow flex-col gap-4 px-4 pb-2">
+        <SignedIn>
+          {routes.map((route, index) => (
+            <Link
+              key={index}
+              href={route.href}
+              onClick={toggleMenu}
+              className={`hover:text-accent-foreground text-muted-foreground inline-flex h-10 w-full items-center text-sm transition-colors sm:w-auto`}
+            >
+              {route.title}
+            </Link>
+          ))}
+        </SignedIn>
 
         <SignedOut>
           <Link href={"/sign-in"} className="w-full sm:w-auto">
@@ -93,15 +92,6 @@ const MobileMenu: React.FC<{ toggleMenu: () => void }> = ({ toggleMenu }) => {
             </Button>
           </Link>
         </SignedOut>
-
-        <SignedIn>
-          <Link href="/dashboard" className="w-full sm:w-auto">
-            <Button variant="default" size="sm" className="w-full">
-              Dashboard
-            </Button>
-          </Link>
-          <UserButton />
-        </SignedIn>
       </div>
       <div className="bg-background/60 h-screen w-full sm:hidden" />
     </div>
